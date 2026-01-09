be1c441e502f1621f3ae54acdf1e0e8724b72695
Jadolleik30 <42230523@students.liu.edu.lb>
2026-01-09 11:53:39 +0200

    Fix server.js and add health route

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,21 @@ const mealsRoutes = require("./routes/meals");
 const goalsRoutes = require("./routes/goals");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.use("/auth", require("./routes/auth"));
-app.use("/meals", require("./routes/meals"));
-app.use("/goals", require("./routes/goals"));
-n 
-const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+
+app.use("/auth", authRoutes);
+app.use("/meals", mealsRoutes);
+app.use("/goals", goalsRoutes);
+
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("Backend running on port", PORT);
 });
